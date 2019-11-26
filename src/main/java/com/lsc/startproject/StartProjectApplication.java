022cbe55e30b3e8909846d86b20f2f667d44de51
halink <lsc0417@aliyun.com>
2019-11-26 12:01:08 +0800

    初始结构

---
+++ src/main/java/com/lsc/startproject/StartProjectApplication.java
@@ -0,0 +1,13 @@
+package com.lsc.startproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lsc/startproject/StartProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsc.startproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
